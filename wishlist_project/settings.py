941bf58e67bbcf6f314662f70590dcccbffcf9f7
Priyanka123Chauhan <priyankachauhan.career@gmail.com>
2025-07-07 23:08:00 +0530

    Update settings.py

--- wishlist_project/settings.py
+++ wishlist_project/settings.py
@@ -23,13 +23,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'wishlist',
     'corsheaders',
-    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
 ]
 
 # Middleware
