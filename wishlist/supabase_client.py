c26dbbc18bd7429d1ff2f2d9b6db9835ea3ff3bd
Priyanka123Chauhan <priyankachauhan.career@gmail.com>
2025-07-07 23:23:20 +0530

    Update supabase_client.py

--- wishlist/supabase_client.py
+++ wishlist/supabase_client.py
@@ -3,4 +3,6 @@ import os
 
 SUPABASE_URL = "https://qjjancckzcwhqjddfzys.supabase.co"
 SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFqamFuY2NremN3aHFqZGRmenlzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA3ODEzMjUsImV4cCI6MjA2NjM1NzMyNX0.YdXu6ErZ0PfCpjvQkAy2Q0kJ2Y-00IBn0agMDzPp3gw"
+DATABASE_URL = "postgresql://postgres:Pdc!!123@db.ehgnkwmrtbfwzzebvvqm.supabase.co:5432/postgres"
+
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
